6954cb22a1a7f0787062020c7337cdfeba2c38ab
Francois Kiesecoms <francoiskiesecoms@MBP-de-Francois.localdomain>
2025-10-20 14:46:50 +0200

    Add external_id to AccountDashboard following upstream pattern
    - Add external_id field to ATTRIBUTE_TYPES, COLLECTION_ATTRIBUTES, SHOW_PAGE_ATTRIBUTES, and FORM_ATTRIBUTES
    - Follow enterprise edition pattern of directly modifying core file
    - Remove complex autoloading workarounds

--- config/initializers/02_citadel_extensions.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# Load Citadel extensions after the application initializes
-Rails.application.config.after_initialize do
-  # Load dashboard extensions
-  require_relative '../../citadel/app/dashboards/extensions/account_dashboard_extension' if defined?(AccountDashboard) && ChatwootApp.citadel?
-end
