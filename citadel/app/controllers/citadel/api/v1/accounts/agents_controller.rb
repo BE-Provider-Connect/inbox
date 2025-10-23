68f3362608a8e62ac934b18e452f5026af6a467b
Francois Kiesecoms <francoiskiesecoms@mbp-de-francois.home>
2025-10-23 09:48:30 +0200

    removing assistant from /Agent endpoint

--- citadel/app/controllers/citadel/api/v1/accounts/agents_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Citadel::Api::V1::Accounts::AgentsController
-  def index
-    super
-    # Include the Assistant singleton in the agents list
-    @assistant = Assistant.instance if Assistant.instance.enabled?
-  end
-end
